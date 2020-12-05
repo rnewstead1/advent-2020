e0038f10c8ba018012a496378605be8cf75e6213
Rachel Normand <racheljnewstead@gmail.com>
2020-12-05 14:32:51 +0000

    day 5 pt 2

--- day5.py
+++ day5.py
@@ -1,5 +1,4 @@
 DATA_FILE = './data/day5.txt'
-seat_ids = []
 
 def bitwise_shift(code, one_symbol):
     number = 0
@@ -9,15 +8,29 @@ def bitwise_shift(code, one_symbol):
         number |= val
     return number
 
-def add_seat_id(seat_code):
-    row = bitwise_shift(seat_code[0:7], 'B')
-    column = bitwise_shift(seat_code[7:10], 'R')
-    seat_id = row * 8 + column
-    seat_ids.append(seat_id)
+def missing_seat_id(ids):
+    ids.sort()
+    my_seat_id = 0
+
+    for i, id in enumerate(ids):
+        next_seat_id = ids[i + 1]
+        if next_seat_id - id != 1:
+          my_seat_id = id + 1
+          break
+
+    return my_seat_id
+
+def all_seat_ids(seat_codes):
+    seat_ids = []
+    for seat_code in seat_codes:
+        row = bitwise_shift(seat_code[0:7], 'B')
+        column = bitwise_shift(seat_code[7:10], 'R')
+        seat_id = row * 8 + column
+        seat_ids.append(seat_id)
+    return seat_ids
 
 with open(DATA_FILE) as file:
-    for line in file:
-        add_seat_id(line.rstrip())
+    seat_ids = all_seat_ids(file)
 
 print(f'Highest seat id: {max(seat_ids)}.')
-
+print(f'My seat id: {missing_seat_id(seat_ids)}')
