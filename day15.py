560360c8af2f7877db7721029556e45ab47ecbcd
Rachel Normand <racheljnewstead@gmail.com>
2020-12-22 23:32:18 +0000

    day 15 pt 2

--- day15.py
+++ day15.py
@@ -17,7 +17,7 @@ def get_next_number(numbers):
         return 0
 
 
-def get_number_at_position(x, initial):
+def get_number_at_slow(x, initial):
     numbers = initial
     while len(numbers) != x:
         next_number = get_next_number(numbers)
@@ -25,5 +25,22 @@ def get_number_at_position(x, initial):
     return numbers[-1]
 
 
-x = 2020
-print(f'{x}th number: {get_number_at_position(x, input_list)}')
+def get_number_at_position(x, numbers, next_number):
+    indexed = {int(i): index for index, i in enumerate(numbers, start=1)}
+    next_index = len(input_list) + 1
+    all_numbers = numbers
+    while next_index != x:
+        all_numbers.append(next_number)
+        if next_number in indexed.keys():
+            last_index = indexed[next_number]
+            indexed[next_number] = next_index
+            next_number = next_index - last_index
+        else:
+            indexed[next_number] = next_index
+            next_number = 0
+        next_index += 1
+    return next_number
+
+
+x = 30000000
+print(f'{x}th number: {get_number_at_position(x, [int(i) for i in input_list], 0)}')
